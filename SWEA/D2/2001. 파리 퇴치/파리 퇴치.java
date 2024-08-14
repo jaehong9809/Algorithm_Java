4feaa1b5974b90947f7cc02af39726441e63005b
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-08-14 15:41:54 +0900

    [D2] Title: 파리 퇴치, Time: 126 ms, Memory: 20,276 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,36 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int test_case = 1; test_case <= T; test_case++) {
-            int N = sc.nextInt();
-            int M = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
             int[][] map = new int[N][N];
             int[][] prefixSum = new int[N + 1][N + 1];
+
             for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    map[i][j] = sc.nextInt();
-                    prefixSum[i+1][j+1] = prefixSum[i][j+1] + prefixSum[i+1][j] + map[i][j] - prefixSum[i][j];
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    prefixSum[i + 1][j + 1] = prefixSum[i][j + 1] + prefixSum[i + 1][j] + map[i][j] - prefixSum[i][j];
                 }
             }
-//
-//            for (int i = 1; i <= N; i++) {
-//                for (int j = 1; j <= N; j++) {
-//                    prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] + map[i - 1][j - 1] - prefixSum[i - 1][j - 1];
-//                }
-//            }
-            int max = -1;
 
+            int max = -1;
 
             for (int i = M; i <= N; i++) {
                 for (int j = M; j <= N; j++) {
                     max = Math.max(prefixSum[i][j] - prefixSum[i - M][j] - prefixSum[i][j - M] + prefixSum[i - M][j - M], max);
                 }
             }
-            System.out.println("#" + test_case + " " + max);
+            sb.append("#" + test_case + " " + max + "\n");
         }
+        System.out.println(sb);
     }
 }
