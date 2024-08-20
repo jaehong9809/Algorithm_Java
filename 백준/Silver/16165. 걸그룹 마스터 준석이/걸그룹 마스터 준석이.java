700b77b1c22ac7a7994cc5ed7fe1fbae47955b5a
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-08-20 23:10:08 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static int n, m;
+    static Map<String, Set<String>> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            String groupName = br.readLine();
+            // TreeSet 탐색 O(log n), HashSet 탐색 O(1)
+            map.put(groupName, new HashSet<>());
+
+            int memberNum = Integer.parseInt(br.readLine());
+            for (int j = 0; j < memberNum; j++) {
+                map.get(groupName).add(br.readLine());
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            String name = br.readLine();
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 1) {
+                for (String groupName : map.keySet()) {
+                    if (map.get(groupName).contains(name)) {
+                        System.out.println(groupName);
+                        break;
+                    }
+                }
+            } else {
+                List<String> members = new ArrayList<>(map.get(name));
+                Collections.sort(members);
+                for (String member : members) {
+                    System.out.println(member);
+                }
+            }
+        }
+
+    }
+}
