48fcea5e5adfc8fda18c1cfe08535bf8a58b4c84
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-09-06 10:09:29 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,445 ms, Memory: 91,468 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -7,63 +7,56 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Solution {
-    static int n,m;
+    static int n, m;
     static int[][] matrix;
+    static int cnt;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int TESTCASE = 1; TESTCASE <= T; TESTCASE++) {
             n = Integer.parseInt(br.readLine());
             m = Integer.parseInt(br.readLine());
-            matrix = new int[n+1][n+1];
+            matrix = new int[n + 1][n + 1];
             for (int i = 0; i < m; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                matrix[a][b]=1;
+                matrix[a][b] = 1;
             }
-            int ans=0;
-            for (int i = 1; i <=n ; i++) {
-                if(gtBFS(i)+ltBFS(i) ==n-1){
-                    ans++;
-                }
+            int ans = 0;
+            for (int i = 1; i <= n; i++) {
+                cnt = 0;
+                gtDFS(i, new boolean[n + 1]);
+                ltDFS(i, new boolean[n + 1]);
+                if(cnt==n-1)ans++;
             }
-            System.out.println("#"+TESTCASE+" "+ans);
+            System.out.println("#" + TESTCASE + " " + ans);
 
         }
     }
-    static int gtBFS(int start){
-        Queue<Integer> queue=new ArrayDeque<>();
-        boolean[] visited=new boolean[n+1];
-        queue.offer(start);visited[start]=true;
-        int cnt=0;
-        while (!queue.isEmpty()) {
-            Integer now = queue.poll();
-            for (int i = 1; i <=n ; i++) {
-                if (!visited[i] && matrix[now][i] != 0) {
-                    queue.offer(i);
-                    visited[i] =true;
-                    cnt++;
-                }
+
+    static int gtDFS(int start, boolean[] visited) {
+        visited[start] = true;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && matrix[start][i] != 0) {
+                gtDFS(i, visited);
+                cnt++;
             }
         }
+
         return cnt;
     }
-    static int ltBFS(int start){
-        Queue<Integer> queue=new ArrayDeque<>();
-        boolean[] visited=new boolean[n+1];
-        queue.offer(start);visited[start]=true;
-        int cnt=0;
-        while (!queue.isEmpty()) {
-            Integer now = queue.poll();
-            for (int i = 1; i <=n ; i++) {
-                if (!visited[i] && matrix[i][now] != 0) {
-                    queue.offer(i);
-                    visited[i] =true;
-                    cnt++;
-                }
+
+    static int ltDFS(int start, boolean[] visited) {
+        visited[start] = true;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i] && matrix[i][start] != 0) {
+                ltDFS(i, visited);
+                cnt++;
             }
         }
+
         return cnt;
     }
 }
