766e2732e7c2a91f5436aa30622f5fc62ad83cf7
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-09-08 16:14:01 +0900

    [Gold II] Title: 문제집, Time: 1248 ms, Memory: 181844 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -4,37 +4,38 @@ class Main {
     static int n, m;
     static ArrayList<Integer>[] graph;
     static int[] indegrees;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n=sc.nextInt();
-        m=sc.nextInt();
-        graph = new ArrayList[n+1];
-        indegrees=new int[n+1];
-        for (int i = 1; i <=n ; i++) {
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new ArrayList[n + 1];
+        indegrees = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < m; i++) {
-            int a= sc.nextInt();
-            int b=sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
             graph[a].add(b);
             indegrees[b]++;
         }
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 1; i <= n; i++) {
-            if(indegrees[i]==0){
+            if (indegrees[i] == 0) {
                 pq.add(i);
             }
         }
 
         while (!pq.isEmpty()) {
             Integer now = pq.poll();
-            System.out.print(now+" ");
+            System.out.print(now + " ");
             for (int i = 0; i < graph[now].size(); i++) {
                 Integer next = graph[now].get(i);
 
-                if(indegrees[next]>0){
+                if (indegrees[next] > 0) {
                     indegrees[next]--;
                 }
 
@@ -45,13 +46,5 @@ class Main {
 
         }
     }
-    static class Node{
-        int easy;
-        int indegree;
 
-        public Node(int easy, int indegree) {
-            this.easy = easy;
-            this.indegree = indegree;
-        }
-    }
 }
