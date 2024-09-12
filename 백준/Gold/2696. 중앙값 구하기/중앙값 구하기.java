aaaee9357c6b76626bb30f89931841efbdd46df3
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-09-12 23:10:54 +0900

    [Gold II] Title: 중앙값 구하기, Time: 352 ms, Memory: 31756 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -4,47 +4,46 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-        
+
         int T = sc.nextInt();
-        
+
         while (T-- > 0) {
             int n = sc.nextInt();
-            int[] data = new int[n];
-            for (int i = 0; i < n; i++) {
+            int[] data = new int[n+1];
+            for (int i = 1; i <= n; i++) {
                 data[i] = sc.nextInt();
             }
-            
-            // 우선순위 큐 설정
+
             PriorityQueue<Integer> left = new PriorityQueue<>(Comparator.reverseOrder());
             PriorityQueue<Integer> right = new PriorityQueue<>();
             ArrayList<Integer> result = new ArrayList<>();
-            
-            for (int i = 0; i < n; i++) {
+
+            if(n<=2){
+                sb.append(1).append("\n").append(data[1]).append("\n");
+                continue;
+            }
+
+            for (int i =1; i <= n; i++) {
                 int num = data[i];
-                
-                // 왼쪽 힙이 비어있거나 num이 왼쪽 힙의 최대값보다 작거나 같으면 왼쪽 힙에 추가
-                if (left.isEmpty() || num <= left.peek()) {
+
+                if (left.isEmpty()|| num <= left.peek()) {
                     left.offer(num);
                 } else {
                     right.offer(num);
                 }
-                
-                // 왼쪽 힙의 크기가 오른쪽 힙보다 2개 이상 클 경우 오른쪽 힙으로 이동
+
                 if (left.size() > right.size() + 1) {
                     right.offer(left.poll());
                 }
-                // 오른쪽 힙의 크기가 왼쪽 힙보다 클 경우 왼쪽 힙으로 이동
                 else if (right.size() > left.size()) {
                     left.offer(right.poll());
                 }
-                
-                // 중앙값을 수집
-                if (i % 2 == 0) {
+
+                if (i % 2 == 1) {
                     result.add(left.peek());
                 }
             }
-            
-            // 결과를 StringBuilder에 추가
+
             sb.append(result.size()).append("\n");
             for (int i = 0; i < result.size(); i++) {
                 sb.append(result.get(i)).append(" ");
@@ -56,7 +55,7 @@ class Main {
                 sb.append("\n");
             }
         }
-        
+
         System.out.print(sb.toString());
     }
 }
