928e2977e8e338ceba525875ba2d7bed02e4838c
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-09-23 22:23:16 +0900

    [Silver III] Title: 블로그, Time: 304 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -10,31 +10,30 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
 
-        int[] data=new int[n+1];
-        st =  new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            data[i] = Integer.parseInt(st.nextToken());
-        }
+        int[] data = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
         int sum = 0;
-
         for (int i = 0; i < x; i++) {
-            sum+=data[i];
+            data[i] = Integer.parseInt(st.nextToken());
+            sum += data[i];
         }
+
         int max = sum;
-        int cnt=1;
+        int cnt = 1;
+
         for (int i = x; i < n; i++) {
-            sum-=data[i-x];
+            sum -= data[i - x];
+            data[i] = Integer.parseInt(st.nextToken());
             sum += data[i];
 
             if (max < sum) {
                 max = sum;
-                cnt=1;
+                cnt = 1;
             } else if (max == sum) {
                 cnt++;
             }
         }
-        if(max==0){
+        if (max == 0) {
             System.out.println("SAD");
             return;
         }
