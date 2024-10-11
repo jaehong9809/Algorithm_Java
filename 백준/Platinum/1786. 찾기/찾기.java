97aedf04179a7dc3e064cdeca0f2c7aad2f73711
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-10-11 13:57:20 +0900

    [Platinum V] Title: 찾기, Time: 2520 ms, Memory: 116252 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,59 +1,68 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String text = br.readLine();  // T 입력
-        String pattern = br.readLine();  // P 입력
-        
-        // 패턴의 시작 위치 탐색
-        List<Integer> result = kmp(text, pattern);
-        
-        // 패턴이 발견된 횟수 출력
-        System.out.println(result.size());
-        
-        // 패턴이 발견된 시작 위치 출력
-        for (int idx : result) {
-            System.out.print(idx + " ");
+class Main{
+    static ArrayList<Integer> res = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String pattern = sc.nextLine();
+        kmp(text, pattern);
+
+        System.out.println(res.size());
+        for (Integer re : res) {
+            System.out.print(re+" ");
         }
     }
-    
-    public static List<Integer> kmp(String text, String pattern) {
-        int[] pi = getPi(pattern);
-        List<Integer> result = new ArrayList<>();
-        int j = 0;
-        
-        // 텍스트에서 패턴 찾기
-        for (int i = 0; i < text.length(); i++) {
-            while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
-                j = pi[j - 1];  // j를 pi 배열 값으로 이동
-            }
-            if (text.charAt(i) == pattern.charAt(j)) {
-                if (j == pattern.length() - 1) {
-                    result.add(i - j + 1);  // 패턴이 일치하는 위치 저장
-                    j = pi[j];  // 다음 검색을 위해 j를 이동
-                } else {
-                    j++;
+
+    public static int[] computeArray(String pattern){
+        int m = pattern.length();
+        int[] lps = new int[m];
+        int len = 0;
+        int i=1;
+
+        lps[0] = 0;
+
+        while (i<m){
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }else {
+                if(len!= 0 ){
+                    len = lps[len-1];
+                }else{
+                    lps[i] = 0;
+                    i++;
                 }
             }
         }
-        return result;
+        return lps;
     }
-    
-    public static int[] getPi(String pattern) {
-        int[] pi = new int[pattern.length()];
-        int j = 0;
-        
-        for (int i = 1; i < pattern.length(); i++) {
-            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
-                j = pi[j - 1];
+
+    public static void kmp(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+        int[] lps = computeArray(pattern);
+
+        int i=0;
+        int j=0;
+        while (i<n){
+            if (pattern.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
             }
-            if (pattern.charAt(i) == pattern.charAt(j)) {
-                pi[i] = ++j;
+
+            if(j==m){
+                res.add(i - j+1);
+                j= lps[j-1];
+            } else if (i < n && pattern.charAt(j) != text.charAt(i)) {
+                if(j!=0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
             }
         }
-        return pi;
     }
 }
