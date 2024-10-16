e39470bff38605bbdc9f3a7242f4837316ca3bf6
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-10-16 23:55:24 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 11.15 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,45 +1,58 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
         int n = jobs.length;
-        PriorityQueue<Job> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost); // 작업의 소요 시간 기준으로 정렬
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]); // 작업 요청 시간 기준으로 정렬
         
-        int time = 0; // 현재 시간
-        int index = 0; // jobs 배열의 인덱스
-        int count = 0; // 완료된 작업 수
-
-        while (count < n) {
-            // 현재 시간까지 요청된 모든 작업을 우선순위 큐에 추가
-            while (index < n && jobs[index][0] <= time) {
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        
+        PriorityQueue<Job> pq =new PriorityQueue<>();
+        
+        int stime=-1;
+        int etime=-1;
+        int time=0;
+        int cnt=0;
+        int index =0;
+        boolean nowworking = false;
+        while(cnt !=n){
+            
+            while (index<n && jobs[index][0]==time) {
                 pq.offer(new Job(jobs[index][0], jobs[index][1]));
                 index++;
+                   
             }
-
-            // 큐가 비어있지 않다면 작업을 하나 꺼내서 처리
-            if (!pq.isEmpty()) {
+            
+            
+            if(!nowworking && !pq.isEmpty()){
+                nowworking = true;
                 Job job = pq.poll();
-                time += job.cost; // 현재 시간에 소요 시간 추가
-                answer += time - job.time; // 요청부터 종료까지 걸린 시간
-                count++;
-            } else {
-                // 큐가 비어있다면 다음 작업의 요청 시간으로 이동
-                time = jobs[index][0];
+                stime = job.t;
+                etime = time + job.c;
             }
+            
+            if(nowworking && etime ==time){
+                answer +=(etime - stime);
+                cnt++;
+                nowworking=false;
+                continue;
+            }
+          
+            time++;
         }
-
-        return answer / n; // 평균값 계산
+        answer/=n;
+        return answer;
     }
-
-    class Job {
-        int time; // 작업 요청 시간
-        int cost; // 작업 소요 시간
-
-        public Job(int time, int cost) {
-            this.time = time;
-            this.cost = cost;
+    
+    class Job implements Comparable<Job>{
+        int t;
+        int c;
+        public Job(int t, int c){
+            this.t = t;
+            this.c = c;
+        }
+        @Override
+        public int compareTo(Job o){
+            return this.c - o.c;
         }
     }
 }
