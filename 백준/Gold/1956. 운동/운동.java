98903ae9f10de962be513b2f1178d5d9a233085c
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-10-28 22:30:53 +0900

    [Gold IV] Title: 운동, Time: 760 ms, Memory: 66300 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int[][] matrix = new int[v + 1][v + 1];
+
+        for (int i = 0; i <= v; i++) {
+            for (int j = 0; j <= v; j++) {
+                if (i == j) continue;
+                matrix[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            matrix[a][b] = c;
+        }
+
+        for (int k = 1; k <= v; k++) {
+            for (int a = 1; a <= v; a++) {
+                for (int b = 1; b <= v; b++) {
+                    matrix[a][b] = Math.min(matrix[a][b], matrix[a][k] + matrix[k][b]);
+                }
+            }
+        }
+        int res = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= v; i++) {
+            for (int j = 1; j <= v; j++) {
+                if (i == j) continue;
+                res = Math.min(res, matrix[i][j] + matrix[j][i]);
+            }
+        }
+
+        System.out.println(res < INF ? res : -1);
+    }
+}
