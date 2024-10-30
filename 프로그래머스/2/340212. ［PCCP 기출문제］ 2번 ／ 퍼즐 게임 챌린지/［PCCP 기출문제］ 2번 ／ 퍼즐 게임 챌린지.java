ff5048b1b7b558a58dfc80125e4240f0ef2dfd72
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-10-30 17:30:58 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 70.99 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,76 @@
+import java.util.*;
+class Solution {
+    static ArrayList<Node> list = new ArrayList<>();
+    static long l ;
+    static int n;
+    public int solution(int[] diffs, int[] times, long limit) {
+        
+        n = diffs.length;
+        l = limit;
+        
+        for(int i=0; i<n; i++){
+            list.add(new Node(diffs[i], times[i]));
+        }
+        
+        int answer = bsc();
+         if (answer == 0) {
+            answer = 1;
+        }
+        return answer;
+    }
+    public int bsc(){
+        int start =0;
+        int end = 100000;
+        int result =0;
+        while(start<end){
+            int mid = (start+end)/2;
+            boolean b = cal(mid);
+            
+            if(b){
+                end = mid;
+                result = mid;
+            }else{
+                start=mid+1;
+            }
+            
+        }
+        return (start + end)/2;
+    }
+    
+    public boolean cal(long level){
+        
+        long time =0;
+        for(int i=0; i<n; i++){
+            Node node = list.get(i);
+            if(node.diff <=level){
+                time +=node.time;
+            }else{
+                if(i==0){
+                    time+=(node.diff - level) * (node.time )+node.time;
+                }else{
+                    time +=(node.diff - level) * (node.time + list.get(i-1).time)+node.time;     
+                }
+                
+            }
+            
+            if(time>l){
+                return false;
+            }
+        }        
+        
+        return true;
+    }
+    
+    
+    class Node  {
+        int diff;
+        int time;
+        public Node(int diff, int time){
+            this.diff = diff;
+            this.time = time;
+        }
+       
+    }
+    
+    
+}
