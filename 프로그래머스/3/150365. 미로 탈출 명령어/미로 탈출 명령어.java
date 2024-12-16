12b183b2c81c5ae7eea86d9de6899862e0019f9b
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2024-12-16 22:24:04 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 6.68 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    static int[] dx = {1, 0, 0, -1};
+    static int[] dy = {0, -1, 1, 0};
+    static String[] dirs = {"d", "l", "r", "u"};
+    
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        String answer = "";
+        int pathNum = (int)Math.abs(r-x)+(int)Math.abs(c-y);
+        if(pathNum>k || (k - pathNum)%2!=0) return "impossible";
+        while(k>0){
+            for(int i=0; i<4; i++){
+                int nx = x+ dx[i];
+                int ny = y+dy[i];
+                if(nx<1 || nx > n || ny<1 || ny>m) continue;
+                if((int)Math.abs(r - nx) + (int)Math.abs(c - ny) > k) continue;
+                x=nx;
+                y = ny;
+                answer+=dirs[i];
+                break;
+            }
+            
+            k--;
+        }
+        
+        return answer;
+    }
+   
+}
