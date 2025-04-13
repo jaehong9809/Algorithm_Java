cb7cb987098594b8018442a65be0d2f7855f28f8
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-04-13 21:42:52 +0900

    [Gold IV] Title: 줄세우기, Time: 192 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(n - max);
+    }
+}
