66e3226e6a23c768c3ad3874e29ce2de03408452
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-09-26 09:33:25 +0900

    [Gold IV] Title: 회사 문화 1, Time: 1144 ms, Memory: 88736 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,51 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
     static int n, m;
-    static int[] dp;
-    static ArrayList<Integer>[] tree;
+    static ArrayList<Integer>[] graph;
+    static int[] point;
+    static boolean[] vi;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        dp = new int[n+1];
-        tree=new ArrayList[n+1];
-
-        for (int i = 1; i <= n; i++) {
-            tree[i] = new ArrayList<>();
+        graph = new ArrayList[n + 1];
+        vi = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
         }
 
+        point = new int[n + 1];
         st = new StringTokenizer(br.readLine());
         st.nextToken();
-
         for (int i = 2; i <= n; i++) {
-            tree[Integer.parseInt(st.nextToken())].add(i);
+            int a = Integer.parseInt(st.nextToken());
+            graph[a].add(i);
         }
-        int[][] q = new int[m][2];
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int node = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            dp[node]+=weight;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            point[a] += b;
         }
-        DFS(1, -1);
+        dfs(1);
 
         for (int i = 1; i <=n ; i++) {
-            System.out.print(dp[i]+" ");
+            System.out.print(point[i]+" ");
         }
-        System.out.println();
+    }
+    static void dfs(int start){
+        if(vi[start]){
+            return;
+        }
+        for (int i = 0; i < graph[start].size(); i++) {
+            int next = graph[start].get(i);
+            if(vi[next]) continue;
+            point[next] +=point[start];
+            dfs(next);
+        }
+    }
 
-    }
-    static void DFS(int x, int prevNode){
-        for (int y : tree[x]) {
-            if(y==prevNode) continue;
-            dp[y] +=dp[x];
-            DFS(y, x);
+    static void bfs(int start) {
+        Queue<Integer> queue = new ArrayDeque<>();
+
+
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            Integer now = queue.poll();
+
+            for (int i = 0; i < graph[now].size(); i++) {
+                Integer next = graph[now].get(i);
+                if (vi[next]) continue;
+                queue.offer(next);
+                point[next] +=point[now];
+            }
         }
     }
+
 }
