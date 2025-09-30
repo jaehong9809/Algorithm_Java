40e15428f357788f90bd875c8970b8aabea8db34
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-09-30 17:50:37 +0900

    [Gold V] Title: 내일 할거야, Time: 1480 ms, Memory: 269524 KB -BaekjoonHub

--- 백준/Gold/7983. 내일 할거야/내일 할거야.java
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.java
@@ -1,21 +1,26 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         ArrayList<Node> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             list.add(new Node(b - a + 1, b, a));
         }
+
         Collections.sort(list, (x, y) -> Integer.compare(x.end, y.end));
+
         int time = list.get(n-1).end+1;
         int gap = 0;
+
         for (int i = n-1; i >=0 ; i--) {
 
             if (list.get(i).end <= time) {
