9c278569cc2428763900e12d6cc2ce708d427f60
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-11-27 10:49:46 +0900

    [Gold IV] Title: 저울, Time: 152 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static int n, m;
+    static int[][] map;
+    static int INF = (int) 1e9;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        map = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) continue;
+                map[i][j] = INF;
+            }
+        }
+
+
+        for (int i = 0; i < m; i++) {
+            String[] s = br.readLine().split(" ");
+            int a = Integer.parseInt(s[0]);
+            int b = Integer.parseInt(s[1]);
+            map[b][a] = 1;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i == j) continue;
+                if (map[i][j] != INF) cnt++;
+                if (map[j][i] != INF) cnt++;
+            }
+            System.out.println(n - cnt - 1);
+        }
+    }
+}
