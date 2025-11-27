6ff8762b5c5ca0593fd2382588819475d84a68c0
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-11-27 14:11:57 +0900

    [Gold IV] Title: 운동, Time: 592 ms, Memory: 66904 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -1,51 +1,54 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
+    static int v, e;
+    static int[][] map;
+    static int INF = (int) 1e9;
 
-    static final int INF = 1_000_000_000;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
 
-        int[][] matrix = new int[v + 1][v + 1];
-
-        for (int i = 0; i <= v; i++) {
-            for (int j = 0; j <= v; j++) {
+        map = new int[v + 1][v + 1];
+        for (int i = 1; i <= v; i++) {
+            for (int j = 1; j <= v; j++) {
                 if (i == j) continue;
-                matrix[i][j] = INF;
+                map[i][j] = INF;
             }
         }
-
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            matrix[a][b] = c;
+            map[a][b] = c;
         }
+
 
         for (int k = 1; k <= v; k++) {
             for (int a = 1; a <= v; a++) {
                 for (int b = 1; b <= v; b++) {
-                    matrix[a][b] = Math.min(matrix[a][b], matrix[a][k] + matrix[k][b]);
+                    map[a][b] = Math.min(map[a][b], map[a][k] + map[k][b]);
                 }
             }
         }
-        int res = Integer.MAX_VALUE;
 
-        for (int i = 1; i <= v - 1; i++) {
-            for (int j = i + 1; j <= v; j++) {
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= v; i++) {
+            int tmpMin = Integer.MAX_VALUE;
+            for (int j = 1; j <= v; j++) {
                 if (i == j) continue;
-                res = Math.min(res, matrix[i][j] + matrix[j][i]);
+                tmpMin = Math.min(tmpMin, map[i][j] + map[j][i]);
             }
+            min = Math.min(min, tmpMin);
         }
-
-        System.out.println(res < INF ? res : -1);
+        System.out.println(min < INF ? min : -1);
     }
 }
