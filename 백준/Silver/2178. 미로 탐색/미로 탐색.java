e61a022e392b4c7880d1376c3b350e8da4e0f791
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2026-03-01 16:23:30 +0900

    [Silver I] Title: 미로 탐색, Time: 184 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,51 +1,53 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Main{
-    static int n;
-    static int m;
-    static int[][] matrix;
+class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n, m;
+    static int[][] map;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n=sc.nextInt();
-        m= sc.nextInt();
-        matrix=new int[n][m];
+        n = sc.nextInt();
+        m = sc.nextInt();
+
         sc.nextLine();
+
+        map = new int[n][m];
+
         for (int i = 0; i < n; i++) {
-            String str = sc.nextLine();
+            String tmp = sc.nextLine();
+
             for (int j = 0; j < m; j++) {
-                matrix[i][j] = str.charAt(j) - '0';
+                map[i][j] = tmp.charAt(j) - '0';
             }
         }
-
-        bfs(0, 0);
-        System.out.println(matrix[n-1][m-1]);
+        bfs();
+        System.out.println(map[n-1][m-1]);
 
     }
 
-    public static void bfs(int startX, int startY){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{startX, startY});
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+    static void bfs() {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0});
+        boolean[][] visited = new boolean[n][m];
+        visited[0][0] = true;
         while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
+            int[] now = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nextX=dx[i]+poll[0];
-                int nextY=dy[i]+poll[1];
-                if(nextX>=0 &&nextX<n&&nextY>=0&&nextY<m){
-                    if(matrix[nextX][nextY]==1){
-                        queue.offer(new int[]{nextX, nextY});
-                        if(nextX==startX&& nextY==startY) continue;
-                        matrix[nextX][nextY] = matrix[poll[0]][poll[1]]+1;
-                    }
-                }
-
+                int nx = dx[i] + now[0];
+                int ny = dy[i] + now[1];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(map[nx][ny]==0)continue;
+                if(visited[nx][ny])continue;
+                queue.offer(new int[]{nx, ny});
+                visited[nx][ny]= true;
+                map[nx][ny] += map[now[0]][now[1]];
             }
         }
-
 
     }
 }
