78bfa73ff8d7991c428ddfea26749eb12952cced
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2026-03-15 23:11:08 +0900

    [Gold III] Title: 줄 세우기, Time: 708 ms, Memory: 50296 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,58 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-class Main {
-    static int n, m;
-    static int[] inDegree;
-    static ArrayList<Integer>[] graph;
-    static ArrayList<Integer> result = new ArrayList<>();
+public class Main {
+	static int n, m;
+	static int[] indegrees;
+	static ArrayList<Integer>[] graph;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        inDegree = new int[n + 1];
-        graph = new ArrayList[n + 1];
+		graph = new ArrayList[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
+		for (int i = 0; i <= n; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
+		indegrees = new int[n + 1];
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            inDegree[b]++;
-        }
-        topologySort();
-        for (Integer i : result) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
+			indegrees[b]++;
+		}
+		func();
+	}
 
-    static void topologySort() {
-        Queue<Integer> queue = new ArrayDeque<>();
-        for (int i = 1; i <= n; i++) {
-            if (inDegree[i] == 0) {
-                queue.offer(i);
-            }
-        }
+	static void func() {
+		Queue<Integer> q = new ArrayDeque<>();
+		for (int i = 1; i <= n; i++) {
+			if (indegrees[i]== 0)
+				q.offer(i);
+		}
 
-        while (!queue.isEmpty()) {
-            Integer now = queue.poll();
-            result.add(now);
-            for (int i = 0; i < graph[now].size(); i++) {
-                int next = graph[now].get(i);
-                inDegree[next]--;
-                if (inDegree[next] == 0) {
-                    queue.offer(next);
-                }
-            }
-        }
-    }
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			System.out.print(now+" ");
+			for (int i = 0; i < graph[now].size(); i++) {
+				int next = graph[now].get(i);
+				
+				if(indegrees[next]>0) indegrees[next]--;
+				
+				if(indegrees[next]==0) {
+					q.offer(next);
+				}
+			}
+
+		}
+
+	}
+
 }
