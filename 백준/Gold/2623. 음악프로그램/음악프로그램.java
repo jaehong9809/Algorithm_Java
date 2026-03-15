5eeac72bda8cb371a1ebd3a38602d2ee0c5352a1
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2026-03-15 23:41:08 +0900

    [Gold III] Title: 음악프로그램, Time: 140 ms, Memory: 15324 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.java
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -1,62 +1,62 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 class Main {
-    static int n, m;
-    static int[] indegree;
-    static ArrayList<Integer>[] graph;
+	static int n, m;
+	static ArrayList<Integer>[] graph;
+	static int[] indegrees;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[n + 1];
-        indegree = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
+		indegrees = new int[n + 1];
 
-        for (int i = 0; i < m; i++) {
-            String[] s = br.readLine().split(" ");
+		graph = new ArrayList[n + 1];
 
-            for (int j = 1; j < s.length - 1; j++) {
-                int now = Integer.parseInt(s[j]);
-                int next = Integer.parseInt(s[j + 1]);
-                graph[next].add(now);
-                indegree[now]++;
-            }
-        }
+		for (int i = 0; i <= n; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
 
-        topologicalSort();
-    }
+		for (int i = 0; i < m; i++) {
+			String[] s = br.readLine().split(" ");
 
-    static void topologicalSort() {
-        Queue<Integer> q = new ArrayDeque<>();
-        Stack<Integer> res = new Stack<>();
-        for (int i = 1; i <= n; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-        while (!q.isEmpty()) {
-            Integer now = q.poll();
-            res.add(now);
-            for (Integer i : graph[now]) {
-                indegree[i]--;
-                if (indegree[i] == 0) {
-                    q.offer(i);
-                }
-            }
-        }
-        if (res.size() != n) {
-            System.out.println(0);
-            return;
-        }
-        while (!res.isEmpty()) {
-            System.out.println(res.pop());
-        }
-    }
+			for (int j = 2; j < s.length; j++) {
+				int now = Integer.parseInt(s[j]);
+				int past = Integer.parseInt(s[j - 1]);
+				graph[past].add(now);
+				indegrees[now]++;
+			}
+		}
+		func();
+	}
+
+	public static void func() {
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		for (int i = 1; i <= n; i++) {
+			if (indegrees[i] == 0)
+				q.offer(i);
+		}
+		List<Integer> list = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			list.add(now);
+			sb.append(now + "\n");
+			for (int i = 0; i < graph[now].size(); i++) {
+				int next = graph[now].get(i);
+				if (indegrees[next] > 0)
+					indegrees[next]--;
+				if (indegrees[next] == 0)
+					q.offer(next);
+			}
+		}
+		if (list.size() != n)
+			System.out.println(0);
+		else
+			System.out.println(sb.toString());
+
+	}
 }
